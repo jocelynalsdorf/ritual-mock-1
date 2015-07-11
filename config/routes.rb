b77e5a268ee9030fd87f04b811c5cb7c8989deff
jocelyn <jfrodsla@gmail.com>
2015-07-11 13:28:15 -0700

    adjust routes os that logged in user sees thei notes as home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
-  devise_for :models
   get 'welcome/index'
-  root 'welcome#index'
+  devise_for :users
   resources :notes
+
+  authenticated :user do
+    root 'notes#index', as: "authenticated_root" #logged in user sees all their notes as homepage
+  end
+
+  root 'welcome#index'
+
+
 end
